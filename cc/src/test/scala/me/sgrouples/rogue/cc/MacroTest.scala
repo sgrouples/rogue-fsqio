b6cec828dd8a596c3d0613577a3bef967549d76b
Marek Zebrowski <marek.zebrowski@gmail.com>
2018-07-18 00:36:09 +0200

    macro type match - in progress

--- cc/src/test/scala/me/sgrouples/rogue/cc/MacroTest.scala
+++ cc/src/test/scala/me/sgrouples/rogue/cc/MacroTest.scala
@@ -4,13 +4,26 @@ import org.scalatest.{ FlatSpec, Matchers }
 import me.sgrouples.rogue.cc._
 import me.sgrouples.rogue.cc.macros.MacroCC._
 import me.sgrouples.rogue.BsonFormats._
-import me.sgrouples.rogue.cc.macros.{ MacroGen }
+import me.sgrouples.rogue.cc.macros.MacroGen
 import me.sgrouples.rogue.cc.macros.BlaDef._
-case class InnSrc(innV: Int = 4)
-case class SourceBson2(name: String = "Name", xx: Int = 8, inn: InnSrc = InnSrc(5))
+import org.bson.types.ObjectId
+object Eni extends Enumeration {
+
+}
+case class InnSrc(oid: ObjectId, innV: Int = 4)
+case class SourceBson2(name: String = "Name", xx: Int = 8,
+  inn: InnSrc = InnSrc(new ObjectId(), 5), opt: Option[Int] = None,
+  arr: Array[Int],
+  outerOid: ObjectId,
+  mp: Map[String, Int],
+  s: Vector[Int],
+  rr: RejectReason.Value = RejectReason.cheater)
+
 class MacroTest extends FlatSpec with Matchers {
 
   "Macro" should "bla cclass" in {
+
+    val k = implicitly[MacroGen[SourceBson2]]
     import me.sgrouples.rogue.cc.macros.BlaDef._
     //val blaString = implicitly[Bla[String]]
     //println(s"bla string is ${blaString}")
@@ -18,16 +31,16 @@ class MacroTest extends FlatSpec with Matchers {
     //val g = implicitly[MacroGen[SourceBson2]]
     //println(g.namesMap())
     //   val sgen = implicitly[MacroGen[SourceBson]]
-    class X extends MCcMetaExt[SourceBson2, X] {
-      val xx = IntField
-      val name = StringField
+    //class X extends MCcMetaExt[SourceBson2, X] {
+    // val xx = IntField
+    //val name = StringField
 
-      //    val a = implicitly[MacroNamesResolver[SourceBson]]
-      //val srcbs = MacroCC.gen[SourceBson]
-      //val srcbs = implicitly[MacroCCMeta[SourceBson]]
-      // val wr = srcbs.write(SourceBson("Name", "url"))
-      //println(s"wr ${wr}")
-    }
+    //    val a = implicitly[MacroNamesResolver[SourceBson]]
+    //val srcbs = MacroCC.gen[SourceBson]
+    //val srcbs = implicitly[MacroCCMeta[SourceBson]]
+    // val wr = srcbs.write(SourceBson("Name", "url"))
+    //println(s"wr ${wr}")
+    //}
 
     assert(true, "OK")
 
--- ccmacros/src/main/scala/me/sgrouples/rogue/cc/macros/CCMacros.scala
+++ ccmacros/src/main/scala/me/sgrouples/rogue/cc/macros/CCMacros.scala
@@ -1,8 +1,15 @@
 package me.sgrouples.rogue.cc.macros
 
-import scala.reflect.macros.whitebox.Context
+import scala.reflect.macros.blackbox.Context
 import scala.language.experimental.macros
+import org.bson.types.ObjectId
+import java.util.UUID
 
+import scala.collection.Seq
+import scala.annotation.implicitNotFound
+import scala.collection.{ GenSeqLike, MapLike }
+
+@implicitNotFound("MacroGen can't generate for ${T}")
 trait MacroGen[T] {
   def namesMap(): Vector[(Int, String)]
 }
@@ -24,9 +31,13 @@ object MacroCC {
 
 class MacroCCGenerator(val c: Context) {
   import c.universe._
+  import definitions.ArrayClass
   def genImpl[T: c.WeakTypeTag]: c.Tree = {
     val tpe = weakTypeOf[T]
     val members = tpe.decls
+    val mapTypeSymbol = typeOf[MapLike[_, _, _]].typeSymbol
+    val iterableTypeSymbol = typeOf[Iterable[_]].typeSymbol
+    //val enumTypeSymbol = typeOf[Value]
 
     val ctorOpt = members.collectFirst {
       case m: MethodSymbol if m.isPrimaryConstructor => m
@@ -39,7 +50,7 @@ class MacroCCGenerator(val c: Context) {
       val companion: c.universe.Symbol = tpe.typeSymbol.companion
 
       if (companion == NoSymbol) {
-        c.abort(c.enclosingPosition, s"Companion symbol not found. Can't find companien for inner classes")
+        c.error(c.enclosingPosition, s"Companion symbol not found for case class ${tpe}. Can't find companion for inner classes")
       }
 
       val defaults = fields.map(_.asTerm).zipWithIndex.map {
@@ -59,11 +70,55 @@ class MacroCCGenerator(val c: Context) {
         val retType = f.typeSignature
         //tpe.decl(name).typeSignature.finalResultType
         println(s"F ${name} / ${decoded} / ${retType}")
+
         //val x = q"BsonRW[c.typeOf[$retType]]"
         //println(s"x ${x}")
         //x
         val tp = f.typeSignature
         val at = appliedType(tp, tp.typeArgs)
+        println(s"Base ${at.baseClasses}")
+        //primitives
+
+        if (at <:< typeOf[Int]) {
+          println(s"Int member ${name}")
+        } else if (at <:< typeOf[Long]) {
+          println(s"Long member ${name}")
+        } else if (at <:< typeOf[Boolean]) {
+          println(s"Boolean member ${name}")
+        } else if (at <:< typeOf[Char]) {
+          println(s"Char member ${name}")
+        } else if (at <:< typeOf[Short]) {
+          println(s"Short member ${name}")
+        } else if (at <:< typeOf[Double]) {
+          println(s"Double member ${name}")
+        } else if (at <:< typeOf[Float]) {
+          println(s"Double member ${name}")
+        } else if (at <:< typeOf[String]) {
+          println(s"String member ${name}")
+        } else if (tp.typeConstructor == typeOf[Option[_]].typeConstructor) {
+          println(s"Option type constructor ${name}")
+        } else if (tp.typeConstructor == typeOf[Array[_]].typeConstructor) {
+          //Array[Byte] is special
+          println(s"Array type constructor ${name}")
+        } else if (at <:< typeOf[org.bson.types.ObjectId]) {
+          println(s"ObjectId subtype ${name}")
+        } else if (at <:< typeOf[java.util.UUID]) {
+          println(s"UUID subtype ${name}")
+        } else if (at.baseClasses.contains(mapTypeSymbol)) {
+          if (!(tp.typeArgs.head <:< typeOf[String])) {
+            c.error(c.enclosingPosition, msg = s"Map key must be of type String for case class ${tpe}.${name}")
+          }
+          println(s"Map type ${name}")
+        } else if (at.baseClasses.contains(iterableTypeSymbol)) {
+          println(s"Iterable like - ${name}")
+        } else {
+          //enum
+          println(s"Other - ... ${name} / ${at} / ${tp.typeConstructor}")
+          //Option[
+          //arrays
+          //Seq
+          //MapLike
+        }
         //val s = q"implicitly[Bla[$tp]]"
         val t = tq"_root_.me.sgrouples.rogue.cc.macros.MacroGen[$tp]"
         println(s"T ${t} type ${t.tpe} , ${at}")
