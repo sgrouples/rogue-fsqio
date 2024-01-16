81ce960bc4a3a283059871d5519d1a7915a6cbcd
Łukasz Krenski <sakulk@gmail.com>
2024-01-16 09:29:19 +0100

    fld as def and allow 'derives MacroBsonFormat'

--- bsonmacros/src/main/scala/me/sgrouples/rogue/cc/macros/MacroBsonFormatDeriving.scala
+++ bsonmacros/src/main/scala/me/sgrouples/rogue/cc/macros/MacroBsonFormatDeriving.scala
@@ -3,7 +3,11 @@ import scala.deriving.*
 import scala.annotation.experimental
 
 trait MacroBsonFormatDeriving:
-  transparent implicit inline def derived[A]: MacroBsonFormat[A] = ${
+  transparent implicit inline def derivedAuto[A]: MacroBsonFormat[A] = ${
+    MacroBsonFormatDerivingImpl.genAuto[A]
+  }
+
+  transparent inline def derived[A]: MacroBsonFormat[A] = ${
     MacroBsonFormatDerivingImpl.gen[A]
   }
 //inline given derived[T](using m: Mirror.Of[T]): MacroBsonFormat[T] = MacroDerivationInline.derived
--- bsonmacros/src/test/scala/me/sgrouples/MacroDerivationTests.scala
+++ bsonmacros/src/test/scala/me/sgrouples/MacroDerivationTests.scala
@@ -9,63 +9,69 @@ import java.time.temporal.ChronoUnit
 import testtags.*
 import org.bson.BsonDocumentWriter
 
-
-case class UX(id:UX.Id, vv:Int)
+case class UX(id: UX.Id, vv: Int)
 object UX:
   type Id = String @@ UX
   object Id:
-    def apply(s:String):Id = tag[UX](s)
- 
-case class OX(id: OX.Id, se:Seq[String])
+    def apply(s: String): Id = tag[UX](s)
+
+case class OX(id: OX.Id, se: Seq[String])
 object OX:
   type Id = ObjectId @@ OX
-    object Id:
-      def apply(s:ObjectId):Id = tag[OX](s)
+  object Id:
+    def apply(s: ObjectId): Id = tag[OX](s)
 
-case class InnerC(i:Int, v:Option[String])
-case class OuterC(i:InnerC, o:Seq[InnerC])
+case class InnerC(i: Int, v: Option[String])
+case class OuterC(i: InnerC, o: Seq[InnerC])
+
+case class DerivesC(i: Int, v: Option[String]) derives MacroBsonFormat
 
 class MacroDerivationTests extends FunSuite:
-    val oid = ObjectId()
-    test("basic types") {
-        roundTrip[Int](1)
-        roundTrip[Long](2L)
-        roundTrip[String]("ala")
-        roundTrip[LocalDateTime](LocalDateTime.now.truncatedTo(ChronoUnit.MILLIS))
-    }
-    test("String & objectId subtypes") {
-        roundTrip[UX.Id](UX.Id("uxid"))
-        roundTrip[OX.Id](OX.Id(ObjectId()))
-    }
-    test("Options") {
-        roundTrip[Option[Int]](Some(7))
-    }
-    test("Seq, List, Vector") {
-        roundTrip[Seq[Int]](Seq(7,2,8))
-        roundTrip[List[Int]](List(7,2,8))
-        roundTrip[Vector[Int]](Vector(7,2,8))
-    }
-    test("Maps") {
-        roundTrip[Map[String, Int]](Map("a" -> 7, "b" -> 2, "c" -> 8))
-    }
-    /*test("Enum") {
+  val oid = ObjectId()
+  test("basic types") {
+    roundTrip[Int](1)
+    roundTrip[Long](2L)
+    roundTrip[String]("ala")
+    roundTrip[LocalDateTime](LocalDateTime.now.truncatedTo(ChronoUnit.MILLIS))
+  }
+  test("String & objectId subtypes") {
+    roundTrip[UX.Id](UX.Id("uxid"))
+    roundTrip[OX.Id](OX.Id(ObjectId()))
+  }
+  test("Options") {
+    roundTrip[Option[Int]](Some(7))
+  }
+  test("Seq, List, Vector") {
+    roundTrip[Seq[Int]](Seq(7, 2, 8))
+    roundTrip[List[Int]](List(7, 2, 8))
+    roundTrip[Vector[Int]](Vector(7, 2, 8))
+  }
+  test("Maps") {
+    roundTrip[Map[String, Int]](Map("a" -> 7, "b" -> 2, "c" -> 8))
+  }
+  /*test("Enum") {
 
     }*/
-    test("case class"){
-        roundTrip[InnerC](InnerC(8, Some("hiyah")))
-    }
-    test("nested case class"){
-        roundTrip[OuterC](OuterC(InnerC(8, Some("hiyah")), Seq(InnerC(1,Some("a")), InnerC(2,None))))
-    }
+  test("case class") {
+    roundTrip[InnerC](InnerC(8, Some("hiyah")))
+  }
+  test("nested case class") {
+    roundTrip[OuterC](
+      OuterC(
+        InnerC(8, Some("hiyah")),
+        Seq(InnerC(1, Some("a")), InnerC(2, None))
+      )
+    )
+  }
 
-    inline def roundTrip[T](v:T)(using t:MacroBsonFormat[T] )=
-        val obj = t.write(v)
-        /*println(s"write ${v}")
+  test("case class with derives") {
+    roundTrip[DerivesC](DerivesC(2, Some("b")))
+  }
+
+  inline def roundTrip[T](v: T)(using t: MacroBsonFormat[T]) =
+    val obj = t.write(v)
+    /*println(s"write ${v}")
         println(s"wrote ${obj}")
         println(s"valid names ${t.validNames()}")*/
-        val r = t.read(obj)
-        assertEquals(v, r, s" for type ${v.getClass}")
-        
-
-
-  
+    val r = t.read(obj)
+    assertEquals(v, r, s" for type ${v.getClass}")
